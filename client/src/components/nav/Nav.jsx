8b1f594d7227ad3eb453a9ca5872a7b9cc18e1c9
John Lopez <john.lopez3695@gmail.com>
2024-03-12 15:50:04 -0400

    Style for nav bar complete

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import "../../styles/nav/nav.css";
 
 function Nav() {
+  const getNavLinkClass = (isActive) => `link ${isActive ? "active-link" : ""}`;
+
   return (
     <header>
       <nav>
@@ -13,14 +15,32 @@ function Nav() {
               src="http://www.sweetclipart.com/multisite/sweetclipart/files/soccer_ball_football_futbol_1.png"
               alt="soccerball"
             />
-            <NavLink to="/">
+            <NavLink
+              to="/"
+              className={({ isActive }) => getNavLinkClass(isActive)}
+            >
               <h1>The Elites</h1>
             </NavLink>
           </div>
           <div className="nav-links">
-            <NavLink to="/">About |</NavLink>
-            <NavLink to="/teams"> Home |</NavLink>
-            <NavLink to="/create-team">Create</NavLink>
+            <NavLink
+              to="/"
+              className={({ isActive }) => getNavLinkClass(isActive)}
+            >
+              About <span className="separator">|</span>
+            </NavLink>
+            <NavLink
+              to="/teams"
+              className={({ isActive }) => getNavLinkClass(isActive)}
+            >
+              Home <span className="separator">|</span>
+            </NavLink>
+            <NavLink
+              to="/create-team"
+              className={({ isActive }) => getNavLinkClass(isActive)}
+            >
+              Create
+            </NavLink>
           </div>
         </div>
       </nav>
