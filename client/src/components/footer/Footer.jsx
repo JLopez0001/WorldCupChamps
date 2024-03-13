eda856a9a5452d6ea1812e0bea6a3a8d986293dc
John Lopez <john.lopez3695@gmail.com>
2024-03-12 22:20:45 -0400

    footer styling modified

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,20 +5,27 @@ import "../../styles/footer/footer.css";
 function Footer() {
   return (
     <footer className="footer-container">
-      <p>Found This Useful?</p>
-      <p>Get To Know The Developer Behind “The Elites”</p>
+      <p className="footer-content">Found This Useful?</p>
+      <p className="footer-content">
+        Get To Know The Developer Behind “The Elites”
+      </p>
       <div>
-        <img
-          className="img-links"
-          src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol.png"
-          alt="github"
-        />
-        <img
-          className="img-links"
-          src="https://pngimg.com/uploads/linkedIn/linkedIn_PNG29.png"
-          alt="linkedIn"
-        />
+        <Link to={"https://github.com/JLopez0001"}>
+          <img
+            className="img-links"
+            src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol.png"
+            alt="github"
+          />
+        </Link>
+        <Link to={"https://www.linkedin.com/in/john-lopez01/"}>
+          <img
+            className="img-links"
+            src="https://pngimg.com/uploads/linkedIn/linkedIn_PNG29.png"
+            alt="linkedIn"
+          />
+        </Link>
       </div>
+      <p>© 2024 The Elites. All Rights Reserved.</p>
     </footer>
   );
 }
